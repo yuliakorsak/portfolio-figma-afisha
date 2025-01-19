05b1be302d3d324ac6454636068ebf94278a31a0
Yulia Ermolaeva <168757304+yuliakorsak@users.noreply.github.com>
2025-01-19 18:59:03 +0300

    Исправлено отображение картинок, доработан footer

--- src/js/ui/Footer.jsx
+++ src/js/ui/Footer.jsx
@@ -24,6 +24,7 @@ export default function Footer() {
           </div>
         </div>
       </div >
+      <div className="footer__copyright">Сайт концертно-экскурсионных программ</div>
     </footer >
   );
 }
